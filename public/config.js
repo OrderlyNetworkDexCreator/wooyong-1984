a7883475fa067e177951b48ef4d2673f4eb26989
Mario Reder <mario.reder@pm.me>
2025-10-09 11:17:08 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/wooyong-1984/",
+  "VITE_SEO_SITE_URL": "https://woowoyo.co.ke",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
